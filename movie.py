06bd49d6cdc6a8fb644bed82153faf8e42fdb4d0
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-29 08:35:03 -0500

    Updated from Brython Server: 1/29/2018 8:35:02 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,14 @@
+#Heather Stafford
+#1/29/18
+#movie.py
 
+age = int(input('Enter your age: '))
+
+if age <= 8:
+    print('You can watch G Rated movies')
+elif age <= 12:
+    print('You can watch PG Rated movies')
+elif age <= 16:
+    print('You can watch PG-13 Rated movies')
+else:
+    print('You can watch R Rated movies')
