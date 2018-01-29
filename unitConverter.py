a26bb454e70907e8aa6789ffff67dc3aef7595fd
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-29 08:48:51 -0500

    Updated from Brython Server: 1/29/2018 8:48:50 AM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,11 @@
+#Heather Stafford
+#1/29/18
+#unitConverter.py
+
+op1 = 'kilometers to Miles'
+op2 = 'Kilograms to Pounds'
+op3 = 'Liters to Gallons'
+op4 = 'Celsius to Fahrenheit'
+
+print('Chose a number 1:' op1, '2:' op2'3:', op3 '4', op4)
 
