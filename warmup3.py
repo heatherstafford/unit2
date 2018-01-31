a7d1e5f705859236f6471c86a9e9aa47a88ef915
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-31 08:20:41 -0500

    Updated from Brython Server: 1/31/2018 8:20:41 AM

--- warmup3.py
+++ warmup3.py
@@ -4,10 +4,12 @@
 
 num = int(input('Enter a number: '))
 
-if num % 2 and num % 3 == 0:
+if num % 2 == 0 and num % 3 == 0:
     print(num,'is divisible by 2 and 3')
 elif num % 3 == 0:
     print(num, 'is divisible by 3')
-else num % 2 == 0:
+elif num % 2 == 0:
     print(num,'is divisible by 2')
+else:
+    print(num, 'is not divisible by 2 or 3')
 
