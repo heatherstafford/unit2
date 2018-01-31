734cac33fcc01a56eec5e699c9e2059bbb5e31d1
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-31 08:29:57 -0500

    Updated from Brython Server: 1/31/2018 8:29:57 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -3,19 +3,21 @@
 #fortuneTeller.py
 
 color = input('Pick red or blue: ')
-number = input('Pick a number from 1-4: ')
+number = int(input('Pick a number from 1 - 4: '))
+
 if color == 'blue' and number == 1:
-    print('Someone will give you food in the next week')
-elif color == blue and number == 2:
-    
-elif color == blue and number == 3:
-    
-elif color == blue and number == 4:
-    
-elif red == blue and number == 1:
-    
-elif red == blue and number == 2:
-    
-elif red == blue and number == 3:
-    
-elif red == blue and number == 4:
+    print('Someone will buy food for you.')
+elif color == 'blue' and number == 2:
+    print('You will have good luck for a week.')
+elif color == 'blue' and number == 3:
+    print('You will finish the quarter with an A.')
+elif color == 'blue' and number == 4:
+    print('You will live to be 100.')
+elif color == 'red' and number == 1:
+    print('Someone will do something nice for you today.')
+elif color == 'red' and number == 2:
+    print('You will not get a lot of sleep tonight.')
+elif color == 'red' and number == 3:
+    print('You will work your dream job.')
+else:
+    print('You will get lots of sleep tonight.')
