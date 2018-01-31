92d3922a94f6202be9dae6b8f6d09e44f7250d7c
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-01-31 08:36:53 -0500

    Updated from Brython Server: 1/31/2018 8:36:52 AM

--- Insulter.py
+++ Insulter.py
@@ -1,1 +1,19 @@
+#Heather Stafford
+#1/31/18
+#insulter.py
 
+from random import randint
+
+input('Enter your name: ')
+
+num = randint(1,5)
+if num == 1:
+    print('You should carry around a plant, to replace all the oxygen you waste.')
+elif num == 2:
+    print('')
+elif num == 3:
+    print('')
+elif num == 4:
+    print('')
+else:
+    print('')
