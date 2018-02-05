865c0c326e76581fe61ba607f4ff442409353a49
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-05 08:32:24 -0500

    Updated from Brython Server: 2/5/2018 8:32:21 AM

--- lastname.py
+++ lastname.py
@@ -4,3 +4,7 @@
 
 lastname = input('Enter your last name: ')
 
+if 'N' <= lastname:
+    print(lastname,'is in the second half of the alphabet')
+else:
+    print(lastname, 'is in the first half of the alphabet')
