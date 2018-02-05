b570bf24ba2a3c09b6cfeefe66f1161da8e92584
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-05 08:39:32 -0500

    Updated from Brython Server: 2/5/2018 8:39:32 AM

--- slope.py
+++ slope.py
@@ -6,6 +6,9 @@ x1 = int(input('x1 ='))
 y1 = int(input('y1 ='))
 x2 = int(input('x2 ='))
 y2 = int(input('y2 ='))
-slope = (y2-y1)/(x2-x1)
-print('The slope is',slope)
-print('The equation of the line is )
+if x2-x1 == 0:
+    print('The slope is underfind')
+else:
+    slope = (y2-y1)/(x2-x1)
+    print('The slope is',slope)
+    print('The equation of the line is' )
