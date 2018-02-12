cae32afb5c58a4f28d7cf26cb15b12468c09b965
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-12 08:28:12 -0500

    Updated from Brython Server: 2/12/2018 8:28:12 AM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,34 @@
+#Heather Stafford
+#2/12/18
+#quiz2.py
 
+firstword = input('Enter a word: ')
+secondword = input('Enter another word: ')
+
+letters1 = int(len(firstword))
+letters2 = int(len(secondword))
+
+if letters1 == letters2 :
+    print('They are the same length.')
+elif letters1 > letters2:
+    print('The first word is longer.')
+else:
+    print('The second word is longer.')
+
+if 'p' in firstword and secondword or 'P' in firstword and secondword:
+    print('Both words have p')
+elif 'p' in firstword or 'P' in firstword:
+    print('The first word has p')
+elif 'p' in secondword or 'P' in secondword:
+    print('The second word has p')
+else:
+    ('Neither words have p')
+
+print('Enter 2 numbers that add up to 12')
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+if num1 + num2 == 12:
+    print('correct')
+else:
+    print('incorrect')
